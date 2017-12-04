965cbdbcabf885b7a87cfab69d6e484e80ef2ee5
Matt Cosand <matt@cosand.net>
2017-12-03 22:45:15 -0800

    Day 3 part 2

--- day-03/Program.cs
+++ day-03/Program.cs
@@ -11,16 +11,41 @@ namespace day_03
     static void Main(string[] args)
     {
       int input = 289326;
+      //int input = 23;
+      int side = 1;
+      while (side * side < input)
+      {
+        side += 2;
+      }
+      int[][] grid = new int[side+2][];
+      for (int i=0;i<grid.Length;i++)
+      {
+        grid[i] = new int[side+2];
+      }
 
       int[][] directions = { new int[] { 1, 0 }, new int[] { 0, 1 }, new int[]{ -1, 0 }, new int[] { 0, -1 } };
+
       int direction = 0;
       int x = 0;
       int y = 0;
+      int origin_x = side / 2 + 1;
+      int origin_y = side / 2 + 1;
       int radius = 0;
-      int value = 1;
+      int val;
 
       do
       {
+        val = 0;
+        for (int i = -1; i < 2; i++)
+        {
+          for (int j = -1; j < 2; j ++)
+          {
+            val += grid[origin_y - y + i][origin_x + x + j];
+          }
+        }
+        grid[origin_y - y][origin_x + x] = val;
+        grid[origin_y][origin_x] = 1;
+
         //Console.WriteLine($"{value:0000} ({x}, {y}). Distance = {Math.Abs(x) + Math.Abs(y)}");
 
         if (x == -y && x >= 0)
@@ -44,12 +69,15 @@ namespace day_03
         x = new_x;
         y = new_y;
 
+        Console.WriteLine(val);
 
 
-        value++;
-      } while (value < 289326);
 
-      Console.WriteLine($"{value} at ({x},{y}), distance = {Math.Abs(x) + Math.Abs(y)}");
+      } while (val < input);
+
+     // Console.WriteLine($"{value} at ({x},{y}), distance = {Math.Abs(x) + Math.Abs(y)}");
+
+      Console.WriteLine(val);      
     }
   }
 }
