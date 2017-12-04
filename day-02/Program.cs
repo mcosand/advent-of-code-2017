3b1c0c7671ea8692f6ff943ea63e74ad2de72f9e
Matt Cosand <matt@cosand.net>
2017-12-03 21:09:02 -0800

    Day 2 part 2

--- day-02/Program.cs
+++ day-02/Program.cs
@@ -11,17 +11,21 @@ namespace day_02
   {
     static void Main(string[] args)
     {
-      string[] sample = { "5 1 9 5",
-"7 5 3",
-"2 4 6 8" };
+      string[] sample = { "5 9 2 8",
+"9 4 7 3",
+"3 8 6 5"
+ };
 
       string[] input = File.ReadLines("input.txt").ToArray();
+      //string[] input = sample;
 
       int sum = 0;
       for (int i = 0; i < input.Length; i++)
       {
-        int[] fields = input[i].Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(f => int.Parse(f)).ToArray();
-        sum += fields.Max() - fields.Min();
+        int[] fields = input[i].Split(new char[] { '\t',' ' }, StringSplitOptions.RemoveEmptyEntries).Select(f => int.Parse(f)).ToArray();
+        var divisor = (from a in fields join b in fields on 1 equals 1 where a > b && (double)(a / b) == (double)a / (double)b select (int)a/b).First(); // where a / b == (double)a / (double)b select a / b).First();
+        Console.WriteLine(divisor);
+        sum += divisor;
       }
       Console.WriteLine("The answer is " + sum);
     }
