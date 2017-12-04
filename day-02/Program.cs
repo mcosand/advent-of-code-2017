850517a0eab9bd766079bea9192502cc20b03e94
Matt Cosand <matt@cosand.net>
2017-12-03 20:58:47 -0800

    day 2 part 1

---
+++ day-02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_02
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] sample = { "5 1 9 5",
+"7 5 3",
+"2 4 6 8" };
+
+      string[] input = File.ReadLines("input.txt").ToArray();
+
+      int sum = 0;
+      for (int i = 0; i < input.Length; i++)
+      {
+        int[] fields = input[i].Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries).Select(f => int.Parse(f)).ToArray();
+        sum += fields.Max() - fields.Min();
+      }
+      Console.WriteLine("The answer is " + sum);
+    }
+  }
+}
