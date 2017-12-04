f14e100bfccd30d6bb3257684f1ab7519c460f59
Matt Cosand <matt@cosand.net>
2017-12-03 22:50:36 -0800

    Day 4 part 1

---
+++ day-04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_04
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int valid = 0;
+      foreach (string line in File.ReadLines("input.txt").Select(f => f.Trim())) {
+        var dict = line.Split(' ').GroupBy(f => f).ToDictionary(f => f.Key, f => f.Count());
+        if (dict.All(f => f.Value == 1)) valid++;
+      }
+      Console.WriteLine(valid);
+    }
+  }
+}
