7a90a9e2d5c5060f021e3a13ed3f681b0b78be62
Matt Cosand <matt@cosand.net>
2017-12-04 23:04:17 -0800

    day 5 part 1

---
+++ day-05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_05
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var offsets = File.ReadAllLines("input.txt").Select(f => int.Parse(f)).ToArray();
+      var pc = 0;
+      var steps = 0;
+      while (pc >= 0 && pc < offsets.Length)
+      {
+        var old = pc;
+        pc = pc + offsets[pc]++;
+        steps++;
+      }
+      Console.WriteLine(steps);
+    }
+  }
+}
