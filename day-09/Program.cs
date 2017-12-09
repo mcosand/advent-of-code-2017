a265af095ded4b6f791d123ce728f5c29549f912
Matt Cosand <matt@cosand.net>
2017-12-08 21:14:28 -0800

    day 9 part 1 - rank 153

---
+++ day-09/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace day_09
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string input = File.ReadAllText("input.txt");
+      //string input = "{{<ab>},{<ab>},{<ab>},{<ab>}}";
+
+      bool garbage = false;
+      int group = 0;
+      int sum = 0;
+
+      for (var i = 0; i< input.Length; i++)
+      {
+        if (input[i] == '!')
+        {
+          i++;
+        }
+        else if (garbage)
+        {
+          if (input[i] == '>') garbage = false;
+        }
+        else if (input[i] == '<')
+        {
+          garbage = true;
+        }
+        else if (input[i] == '{')
+        {
+          group++;
+        }
+        else if (input[i] == '}')
+        {
+          sum += group--;
+        }
+      }
+      Console.WriteLine(group);
+    }
+  }
+}
