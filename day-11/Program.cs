fff7be2146929f6bb7f0944f9a3e0a287c1fad5d
Matt Cosand <matt@cosand.net>
2017-12-10 22:13:53 -0800

    day 11 - with pictures!

--- day-11/Program.cs
+++ day-11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 
@@ -8,11 +9,40 @@ namespace day_11
   {
     static void Main(string[] args)
     {
+      int minX = 0, minY = 0, maxX = 0, maxY = 0;
+
+      WalkSteps((x, y) =>
+      {
+        minX = Math.Min(x, minX);
+        minY = Math.Min(y, minY);
+        maxX = Math.Max(x, maxX);
+        maxY = Math.Max(y, maxY);
+      });
+
+      using (var b = new Bitmap(maxX - minX, maxY - minY))
+      {
+        using (Graphics g = Graphics.FromImage(b))
+        {
+          g.Clear(Color.White);
+          g.FillEllipse(new SolidBrush(Color.Red), -minX - 5, -minY - 5, 10, 10);
+          var pen = new Pen(Color.Black);
+          int x = 0;
+          int y = 0;
+          WalkSteps((x2, y2) =>
+          {
+            g.DrawLine(pen, x - minX, y - minY, x2 - minX, y2 - minY);
+            x = x2;
+            y = y2;
+          });
+        }
+        b.Save("output.png");
+      }
+    }
+
+    public static void WalkSteps(Action<int, int> onStep)
+    {
       int x = 0;
       int y = 0;
-
-      int max = 0;
-
       foreach (var step in File.ReadAllText("input.txt").Trim().Split(',').Select(f => f.Trim()))
       {
         switch (step)
@@ -38,9 +68,8 @@ namespace day_11
             x -= 1;
             break;
         }
-        max = Math.Max(max, GetDistance(x, y));
+        onStep(x, y);
       }
-      Console.WriteLine(max);
     }
 
     static int GetDistance(int x, int y)
