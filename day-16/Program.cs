287737bf7bf2a1bbd655887a87cca08270cd62d9
Matt Cosand <matt@cosand.net>
2017-12-15 21:58:45 -0800

    day 16 part 2 (374th - double ugh. Fingers won't type what I'm thinking and the first optimization didn't account for lookup by letters)

--- day-16/Program.cs
+++ day-16/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Linq;
 
 namespace day_16
 {
@@ -23,11 +19,39 @@ namespace day_16
         progs[i] = (char)('a' + i);
       }
 
+
+      Dictionary<string, string> cache = new Dictionary<string, string>();
+
+      int round = 0;
+      for (round = 0; round < 1000000000; round++)
+      {
+        string before = string.Join("", progs);
+        if (cache.ContainsKey(before))
+        {
+          break;
+        }
+        RunRound(steps, progs);
+        cache.Add(before, string.Join("", progs));
+      }
+
+      int remainder = 1000000000 % round;
+      for (var i = 0;i<remainder; i++)
+      {
+        RunRound(steps, progs);
+      }
+
+      var answer = string.Join("", progs);
+      Console.WriteLine($"{answer}, with period of {cache.Count} steps");
+    }
+
+    private static void RunRound(string[] steps, char[] progs)
+    {
       foreach (var step in steps)
       {
         if (step[0] == 's')
         {
-          for (int j = 0; j < int.Parse(step.Substring(1)); j++) {
+          for (int j = 0; j < int.Parse(step.Substring(1)); j++)
+          {
             char temp = progs[progs.Length - 1];
             for (int i = progs.Length - 1; i > 0; i--)
             {
@@ -35,7 +59,8 @@ namespace day_16
             }
             progs[0] = temp;
           }
-        } else if (step[0] == 'x')
+        }
+        else if (step[0] == 'x')
         {
           var match = Regex.Match(step, "x(\\d+)/(\\d+)");
           char temp = progs[int.Parse(match.Groups[1].Value)];
@@ -46,19 +71,18 @@ namespace day_16
         {
           int left = 0;
           int right = 0;
-          for (int i=0;i<progs.Length;i++)
+          for (int i = 0; i < progs.Length; i++)
           {
             if (progs[i] == step[1]) { left = i; }
-            if (progs[i] == step[3]) { right = i;}
+            if (progs[i] == step[3]) { right = i; }
           }
 
           char temp = progs[left];
           progs[left] = progs[right];
           progs[right] = temp;
         }
-      }
 
-      string answer = string.Join("", progs);
+      }
     }
   }
 }
