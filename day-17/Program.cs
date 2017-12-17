633a78f3a4af4bded37e7f62611a76bf5394eacb
Matt Cosand <matt@cosand.net>
2017-12-16 21:56:20 -0800

    day 17 part 2 - brute force. Too slow!!

--- day-17/Program.cs
+++ day-17/Program.cs
@@ -14,18 +14,44 @@ namespace day_17
       var input = 329;
       //var input = 3;
 
+      int answer = Try2(input);
+      Console.WriteLine(answer);
+    }
+
+    private static int Try1(int input)
+    {
       var buffer = new Node { Value = 0 };
       buffer.Next = buffer;
+      var zero = buffer;
 
-      for (int i = 1; i < 2018; i++)
+      for (int i = 1; i < 50000000; i++)
       {
+        if (i % 1000000 == 0) Console.WriteLine(i);
+
         for (int j = 0; j < input; j++) { buffer = buffer.Next; }
 
         var node = new Node { Value = i, Next = buffer.Next };
         buffer.Next = node;
         buffer = buffer.Next;
       }
-      var answer = buffer.Next.Value;
+
+      var answer = zero.Next.Value;
+      return answer;
+    }
+
+    static int Try2(int input) {
+      List<int> buffer = new List<int> { 0 };
+      int zeroIndex = 0;
+      int cursor = 0;
+
+      for (var i = 1; i < 50000000; i++)
+      {
+        if (i % 1000000 == 0) Console.WriteLine(i);
+        cursor = (cursor + input) % buffer.Count;
+        buffer.Insert(cursor++, i);
+        if (zeroIndex >= cursor) zeroIndex++;
+      }
+      return buffer[(zeroIndex + 1) % buffer.Count];
     }
   }
 
