319002b6ac632b47ffaf0f38d80f361ee78364db
Matt Cosand <matt@cosand.net>
2017-12-16 22:28:10 -0800

    day 17 part 2 - implementation of someone's faster answer

--- day-17/Program.cs
+++ day-17/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace day_17
 {
@@ -12,52 +7,17 @@ namespace day_17
     static void Main(string[] args)
     {
       var input = 329;
-      //var input = 3;
 
-      int answer = Try2(input);
-      Console.WriteLine(answer);
-    }
-
-    private static int Try1(int input)
-    {
-      var buffer = new Node { Value = 0 };
-      buffer.Next = buffer;
-      var zero = buffer;
-
-      for (int i = 1; i < 50000000; i++)
-      {
-        if (i % 1000000 == 0) Console.WriteLine(i);
-
-        for (int j = 0; j < input; j++) { buffer = buffer.Next; }
-
-        var node = new Node { Value = i, Next = buffer.Next };
-        buffer.Next = node;
-        buffer = buffer.Next;
-      }
-
-      var answer = zero.Next.Value;
-      return answer;
-    }
-
-    static int Try2(int input) {
-      List<int> buffer = new List<int> { 0 };
-      int zeroIndex = 0;
-      int cursor = 0;
+      var answer = 0;
+      var cursor = 0;
 
       for (var i = 1; i < 50000000; i++)
       {
-        if (i % 1000000 == 0) Console.WriteLine(i);
-        cursor = (cursor + input) % buffer.Count;
-        buffer.Insert(cursor++, i);
-        if (zeroIndex >= cursor) zeroIndex++;
+        cursor = (cursor + input) % i;
+        if (cursor == 0) answer = i;
+        cursor++;        
       }
-      return buffer[(zeroIndex + 1) % buffer.Count];
+      Console.WriteLine(answer);
     }
-  }
-
-  class Node
-  {
-    public Node Next { get; set; }
-    public int Value { get; set; }
   }
 }
