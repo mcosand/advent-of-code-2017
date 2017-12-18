03d328540b10261ec93b308df39bed08ed36efd5
Matt Cosand <matt@cosand.net>
2017-12-17 23:41:57 -0800

    day 18 part 2

--- day-18/Program.cs
+++ day-18/Program.cs
@@ -8,61 +8,94 @@ namespace day_18
 {
   class Program
   {
-    Match[] list = File.ReadAllLines("input.txt").Select(f => f.Trim()).Where(f => !string.IsNullOrWhiteSpace(f)).Select(f => Regex.Match(f, "^([a-z]{3}) ([^ ]+) ?([^ ]*)$")).ToArray();
+    Match[] list;
+    int id;
     long pc = 0;
     Dictionary<string, long> registers = new Dictionary<string, long>();
-    long freq = 0;
+    public long sent = 0;
+    bool log = true;
+
+    Queue<long> queue = new Queue<long>();
+    Queue<long> sendQueue;
+    Status status = Status.SendNext;
+
 
     static void Main(string[] args)
     {
-      new Program().Run();
+      var list = File.ReadAllLines("input.txt").Select(f => f.Trim()).Where(f => !string.IsNullOrWhiteSpace(f)).Select(f => Regex.Match(f, "^([a-z]{3}) ([^ ]+) ?([^ ]*)$")).ToArray();
+      var zero = new Program(list, 0);
+      var one = new Program(list, 1) { sendQueue = zero.queue };
+      zero.sendQueue = one.queue;
+
+      do
+      {
+        if (zero.status != Status.Terminate) zero.Run();
+        if (one.status != Status.Terminate) one.Run();
+
+      } while (zero.status == Status.SendNext || one.status == Status.SendNext);
+      var answer = one.sent;
+      Console.WriteLine(answer);
+    }
+
+    public Program(Match[] list, int id)
+    {
+      this.id = id;
+      this.list = list;
+      registers.Add("p", id);
     }
 
     void Run()
     {
-      bool log = true;
-
-      while (pc >= 0 && pc < list.Length)
+      if (pc < 0 || pc >= list.Length)
       {
-        var inst = list[pc++].Groups;
-        if (log) Console.Write(inst[0].Value);
-        switch (inst[1].Value)
-        {
-          case "set":
-            Write(inst[2].Value, Read(inst[3].Value));
-            break;
-          case "snd":
-            freq = Read(inst[2].Value);
-            break;
-          case "add":
-            Write(inst[2].Value, Read(inst[2].Value) + Read(inst[3].Value));
-            break;
-          case "mul":
-            Write(inst[2].Value, Read(inst[2].Value) * Read(inst[3].Value));
-            break;
-          case "mod":
-            Write(inst[2].Value, Read(inst[2].Value) % Read(inst[3].Value));
-            break;
-          case "rcv":
-            if (Read(inst[2].Value) != 0)
-            {
-              Console.WriteLine("Recovered " + freq);
-              break;
-              // recover frequency
-            }
-            break;
-          case "jgz":
-            if (Read(inst[2].Value) > 0)
-            {
-              pc += Read(inst[3].Value) - 1;
-              if (log) Console.Write($" jumped to {pc}");
-            }
-            break;
-          default:
-            throw new NotImplementedException();
-        }
-        if (log) Console.WriteLine(" " + string.Join(" ", registers.Select(f => $"{f.Key}={f.Value}")));
+        status = Status.Terminate;
+        return;
       }
+
+      var inst = list[pc++].Groups;
+      if (log) Console.Write(id +  " " + inst[0].Value);
+      status = Status.SendNext;
+
+      switch (inst[1].Value)
+      {
+        case "set":
+          Write(inst[2].Value, Read(inst[3].Value));
+          break;
+        case "snd":
+          sendQueue.Enqueue(Read(inst[2].Value));
+          sent++;
+          break;
+        case "add":
+          Write(inst[2].Value, Read(inst[2].Value) + Read(inst[3].Value));
+          break;
+        case "mul":
+          Write(inst[2].Value, Read(inst[2].Value) * Read(inst[3].Value));
+          break;
+        case "mod":
+          Write(inst[2].Value, Read(inst[2].Value) % Read(inst[3].Value));
+          break;
+        case "rcv":
+          if (queue.Count > 0)
+          {
+            Write(inst[2].Value, queue.Dequeue());
+          }
+          else
+          {
+            pc--;
+            status = Status.Waiting;
+          }
+          break;
+        case "jgz":
+          if (Read(inst[2].Value) > 0)
+          {
+            pc += Read(inst[3].Value) - 1;
+            if (log) Console.Write($" jumped to {pc}");
+          }
+          break;
+        default:
+          throw new NotImplementedException();
+      }
+      if (log) Console.WriteLine(" " + string.Join(" ", registers.Select(f => $"{f.Key}={f.Value}")));
     }
 
     long Read(string r)
@@ -89,5 +122,12 @@ namespace day_18
         registers.Add(r, value);
       }
     }
+  }
+
+  enum Status
+  {
+    SendNext,
+    Terminate,
+    Waiting
   }
 }
