9c2378f2a3c49d6a7e5345506f3dd1e87e9c354a
Matt Cosand <matt@cosand.net>
2017-12-21 00:02:32 -0800

    day 21 part 1 A resulting 2x2 collection of 3x3 grids has to enter the next iteration as a 3x3 collection of 2x2 grids, per the documented process

--- day-21/Program.cs
+++ day-21/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using System.Linq;
 
 
 namespace day_21
@@ -17,6 +15,7 @@ namespace day_21
       rules = File.ReadAllLines("input.txt").Select(f => f.Split(new[] { " => " }, StringSplitOptions.None)).ToDictionary(f => f[0], f=> f[1]);
 
       List<string> grids = new List<string> { ".#./..#/###" };
+      string theGrid = grids[0];
 
       int answer = 0;
       for (var c = 0; c < 5; c++)
@@ -25,61 +24,45 @@ namespace day_21
         List<string> nextGrids = new List<string>();
         for (var g = 0; g < grids.Count; g++)
         {
-          if (!RuleSearch(grids, nextGrids, g))
-          {
-            Console.WriteLine("Can't find rule for " + grids[g]);
-          }
+          nextGrids.Add(RuleSearch(grids[g]));
         }
-        grids = nextGrids;
-        //answer = grids.Sum(f => f.ToCharArray().Where(g => g == '#').Count());
+        theGrid = MergeGrid(nextGrids);
+        grids = SplitGrid(theGrid);
         answer = string.Join("", grids).Replace(".", "").Replace("/", "").Length;
       }
       
     }
 
-    private static bool RuleSearch(List<string> grids, List<string> nextGrids, int g)
+    private static string RuleSearch(string grid)
     {
-      var orig = grids[g];
+      var orig = grid;
       for (var f = 0; f < 3; f++)
       {
+        grid = orig;
         if (f == 0)
         {
         }
         else if (f == 1)
         {
-          grids[g] = VFlipGrid(grids[g]);
+          grid = VFlipGrid(grid);
         }
         else
         {
-          grids[g] = HFlipGrid(grids[g]);
+          grid = HFlipGrid(grid);
         }
 
 
         for (var i = 0; i < 4; i++)
         {
-          //Console.WriteLine(grids[g]);
-          if (rules.TryGetValue(grids[g], out string match))
+          if (rules.TryGetValue(grid, out string match))
           {
-            dumpMatch(orig, grids[g]);
-            if (match.Length == 11)
-            {
-              // new grid is 3x3;
-              nextGrids.Add(match);
-            }
-            else
-            {
-              nextGrids.Add(new string(new[] { match[0], match[1], '/', match[5], match[6] }));
-              nextGrids.Add(new string(new[] { match[2], match[3], '/', match[7], match[8] }));
-              nextGrids.Add(new string(new[] { match[10], match[11], '/', match[15], match[16] }));
-              nextGrids.Add(new string(new[] { match[12], match[13], '/', match[17], match[18] }));
-            }
-            return true;
-            // do split
+            dumpMatch(orig, grid);
+            return match;
           }
-          grids[g] = RotateGrid(grids[g]);
+          grid = RotateGrid(grid);
         }
       }
-      return false;
+      throw new ApplicationException("Rule not found");
     }
 
     static int[] rotate3 = new int[] { 8, 4, 0, 3, 9, 5, 1, 7, 10, 6, 2, 11 };
@@ -121,12 +104,47 @@ namespace day_21
       Console.WriteLine();
     }
 
-    static void dumpAll(List<string> grids)
+    static List<string> SplitGrid(string grid)
     {
-      for (var i=0;i<Math.Sqrt(grids.Count);i++)
+      var lines = grid.Split('/');
+      var gridSize = lines.Length % 2 == 0 ? 2 : 3;
+      var splits = new List<string>();
+      for (var i = 0; i < lines.Length; i += gridSize)
       {
-
+        for (var j = 0; j < lines.Length; j += gridSize)
+        {
+          var counter = gridSize == 2 ? new[] { 0, 1 } : new int[] { 0, 1, 2 };
+          splits.Add(string.Join("/", counter.Select(y => new string(lines[j + y].Skip(i).Take(gridSize).ToArray()))));
+        }
       }
+      return splits;
+    }
+
+    static string MergeGrid(List<string> grids)
+    {
+      if (grids.Count == 1) return grids[0];
+
+      int srcSize = grids[0].IndexOf('/');
+      int gridsOnSide = (int)Math.Sqrt(grids.Count);
+      int targetSize = gridsOnSide * srcSize;
+      
+
+      var rows = new List<string>();
+
+      for (var outerY = 0; outerY < gridsOnSide; outerY++)
+      {
+        for (var innerY = 0; innerY < srcSize; innerY++)
+        {
+          rows.Add(string.Join("", Enumerable.Range(0,gridsOnSide)
+            .Select(g => g + (outerY * gridsOnSide))
+            .Select(g => grids[g].Split('/')[innerY])
+
+            ));
+        }
+      }
+      
+
+      return string.Join("/", rows);
     }
   }
 }
