747e8a51ebff83796f14b502301736e8275405b3
Matt Cosand <matt@cosand.net>
2017-12-22 23:17:20 -0800

    day 23 part 2 - got stuck until I called my wife over and explained it to her. uses a prime check from stack overflow was still flubbing the wrapping logic, so I left it as the assembly.

--- day-23/Program.cs
+++ day-23/Program.cs
@@ -8,107 +8,83 @@ namespace day_23
 {
   class Program
   {
-    Match[] list;
-    int id;
+    Match[] list = File.ReadAllLines("input.txt").Select(f => f.Trim()).Where(f => !string.IsNullOrWhiteSpace(f)).Select(f => Regex.Match(f, "^([a-z]{3}) ([^ ]+) ?([^ ]*)$")).ToArray();
     long pc = 0;
     Dictionary<string, long> registers = new Dictionary<string, long>();
-    public long sent = 0;
-    bool log = true;
-
-    Queue<long> queue = new Queue<long>();
-    Queue<long> sendQueue;
-    Status status = Status.SendNext;
-
 
     static void Main(string[] args)
     {
-      var list = File.ReadAllLines("input.txt").Select(f => f.Trim()).Where(f => !string.IsNullOrWhiteSpace(f)).Select(f => Regex.Match(f, "^([a-z]{3}) ([^ ]+) ?([^ ]*)$")).ToArray();
-      var zero = new Program(list, 0);
-      //var one = new Program(list, 1) { sendQueue = zero.queue };
-      //zero.sendQueue = one.queue;
-
-      do
-      {
-        if (zero.status != Status.Terminate) zero.Run();
-        //if (one.status != Status.Terminate) one.Run();
-
-      } while (zero.status == Status.SendNext/* || one.status == Status.SendNext*/);
-      //var answer = one.sent;
-     // Console.WriteLine(answer);
+      var p = new Program();
+      p.Run();
+      Console.WriteLine(p.registers["h"]);
     }
+
+    static int isPrime(long number)
+    {
+      if (number == 1) return 0;
+      if (number == 2) return 1;
+
+      for (int i = 2; i <= Math.Ceiling(Math.Sqrt(number)); ++i)
+      {
+        if (number % i == 0) return 0;
+      }
+
+      return 1;
+
+    }
+
 
     static int mulTimes = 0;
 
-    public Program(Match[] list, int id)
+    public Program()
     {
-      this.id = id;
-      this.list = list;
-      registers.Add("p", id);
+      registers.Add("a", 1);
     }
 
     void Run()
     {
-      if (pc < 0 || pc >= list.Length)
+      while (true)
       {
-        status = Status.Terminate;
-        return;
-      }
+        if (pc < 0 || pc >= list.Length)
+        {
+          return;
+        }
 
-      var inst = list[pc++].Groups;
-      if (log) Console.Write(id + " " + inst[0].Value);
-      status = Status.SendNext;
+        if (list[pc].Groups[0].Value == "set d 2")
+        {
+          Console.WriteLine("!! short circuit.");
+          Write("f", isPrime(Read("b")));
+          pc = 24;
+        }
 
-      switch (inst[1].Value)
-      {
-        case "set":
-          Write(inst[2].Value, Read(inst[3].Value));
-          break;
-        //case "snd":
-        //  sendQueue.Enqueue(Read(inst[2].Value));
-        //  sent++;
-        //  break;
-        //case "add":
-        //  Write(inst[2].Value, Read(inst[2].Value) + Read(inst[3].Value));
-        //  break;
-        case "sub":
-          Write(inst[2].Value, Read(inst[2].Value) - Read(inst[3].Value));
-          break;
-        case "mul":
-          Write(inst[2].Value, Read(inst[2].Value) * Read(inst[3].Value));
-          mulTimes++;
-          break;
-        //case "mod":
-        //  Write(inst[2].Value, Read(inst[2].Value) % Read(inst[3].Value));
-        //  break;
-        //case "rcv":
-        //  if (queue.Count > 0)
-        //  {
-        //    Write(inst[2].Value, queue.Dequeue());
-        //  }
-        //  else
-        //  {
-        //    pc--;
-        //    status = Status.Waiting;
-        //  }
-        //  break;
-        //case "jgz":
-        //  if (Read(inst[2].Value) > 0)
-        //  {
-        //    pc += Read(inst[3].Value) - 1;
-        //    if (log) Console.Write($" jumped to {pc}");
-        //  }
-        //  break;
-        case "jnz":
-          if (Read(inst[2].Value) != 0)
-          {
-            pc += Read(inst[3].Value) - 1;
-            if (log) Console.Write($" jumped to {pc}");
-          }
-          break;
-        default:
-          throw new NotImplementedException();
+        var inst = list[pc++].Groups;
+        Console.Write(pc + " " + inst[0].Value);
+
+
+        switch (inst[1].Value)
+        {
+          case "set":
+            Write(inst[2].Value, Read(inst[3].Value));
+            break;
+          case "sub":
+            Write(inst[2].Value, Read(inst[2].Value) - Read(inst[3].Value));
+            break;
+          case "mul":
+            Write(inst[2].Value, Read(inst[2].Value) * Read(inst[3].Value));
+            mulTimes++;
+            break;
+          case "jnz":
+            if (Read(inst[2].Value) != 0)
+            {
+              pc += Read(inst[3].Value) - 1;
+              Console.Write($" jumped to {pc}");
+            }
+            break;
+          default:
+            throw new NotImplementedException();
+        }
+        Console.WriteLine(" " + string.Join(" ", registers.Select(f => $"{f.Key}={f.Value}")));
       }
-      if (log) Console.WriteLine(" " + string.Join(" ", registers.Select(f => $"{f.Key}={f.Value}")));
     }
 
     long Read(string r)
@@ -136,12 +112,5 @@ namespace day_23
         registers.Add(r, value);
       }
     }
-  }
-
-  enum Status
-  {
-    SendNext,
-    Terminate,
-    Waiting
   }
 }
